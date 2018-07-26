9fffdc6b0333562b9caf24388f98d27693b7a1c7
Marco Ferraioli <overflowsystem03@gmail.com>
2018-07-26 10:07:38 +0200

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+
+var ca = [fs.readFileSync(process.env.MONGO_CERT)];
+
+var options = {
+    ssl: true,
+    sslValidate: true,
+    sslCA:ca,
+};
+
+var _db;
+
+module.exports.connectDB = (callback) => {
+    MongoClient.connect(process.env.MONGODB_URL, options, function(err, db) {
+        _db = db;
+        return callback(err);
+    });
+};
+
+
+module.exports.getDB = (dbName) => {
+    return _db.db(dbName);
+};
+
+module.exports.disconnectDB = () => {
+    _db.close()
+};
+module.exports.ObjectId = require('mongodb').ObjectId;
